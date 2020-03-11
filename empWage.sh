e639f293a4b1a3d0f008b8a25cf7a2490c900f60
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-03-11 19:16:59 +0530

    MODIFIED:Finded employee salary

---
+++ empWage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+#WE ARE TAKINH ONE VARIABLE
+isPresent=1
+randomCheck=$((RANDOM%2))
+if [ $isPresent -eq $randomCheck	]
+then
+	empRatePerHr=20
+	empHrs=8
+	salary=$(($empHrs*$empRatePerHr))
+else
+	salary=0
+fi
+echo "$salary"
+
