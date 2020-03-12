ee7cef4d9698a98defd47bfab028eaff5566f767
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-03-12 10:23:11 +0530

    MODIFIED:Finded the employee salary

--- ProblemStart/empWage.sh
+++ ProblemStart/empWage.sh
@@ -2,10 +2,14 @@
 #WE ARE TAKINH ONE VARIABLE
 isPresent=1
 randomCheck=$((RANDOM%2))
-if [ $isPresent -eq $randomCheck	]
+if [ $isPresent -eq $randomCheck ]
 then
-	echo "Employee is present"
+   empRatePerHr=20
+   empHrs=8
+   salary=$(($empHrs*$empRatePerHr))
 else
-	echo "Employee is abscent"
+   salary=0
 fi
+echo "$salary"
+
 
