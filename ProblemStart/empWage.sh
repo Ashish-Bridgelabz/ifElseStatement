f46f874b48b764291c2a26e3f836243eabf6b4f0
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-03-12 11:54:13 +0530

    MODIFIED:Employee is present fulltime or parttime

--- ProblemStart/empWage.sh
+++ ProblemStart/empWage.sh
@@ -1,15 +1,19 @@
 #!/bin/bash -x
-#WE ARE TAKINH ONE VARIABLE
-isPresent=1
-randomCheck=$((RANDOM%2))
-if [ $isPresent -eq $randomCheck ]
+#WE ARE TAKING VARIABLE
+isPartTime=1
+isFullTime=2
+empRatePerHr=20
+randomCheck=$((RANDOM%3))
+if [ $isFullTime -eq $randomCheck ]
 then
-   empRatePerHr=20
    empHrs=8
-   salary=$(($empHrs*$empRatePerHr))
+elif [ $isPartTime -eq $randomCheck ]
+then
+	empHrs=4
 else
-   salary=0
+	empHrs=0
 fi
+salary=$(($empHrs*$empRatePerHr))
 echo "$salary"
 
 
